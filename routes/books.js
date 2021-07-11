1551444667ddc0cd855e332d024cf78f9b55c686
Jason Ziegler <jason@insightfulautomation.com>
2021-07-11 11:05:13 -0500

    added update book route

--- routes/books.js
+++ routes/books.js
@@ -30,11 +30,49 @@ router.get('/new', function(req, res, next) {
   res.render('books-new', { title: 'Add a New Book' });
 });
 
-/* GET books/id page. */
-router.get('/:id', function(req, res, next) {
-  res.render('book', { title: 'Update Book' });
-});
+/* POST books/new page. */
+router.post('/new', asyncHandler(async (req, res) => {
+  
+  console.log(req.body);
+  const book = await Book.create(req.body);
+  
+  res.redirect("/books/" + book.id); // will eventually add article.id to path
+}));
 
+/* GET books/id page. */
+router.get('/:id', asyncHandler( async (req, res, next) => {
+  // use id to find book in db
+  const book = await Book.findByPk(req.params.id);
+  // pass book to render template
+  if (book) {
+    res.render('book', { book});
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* POST books/id route. */
+router.post('/:id', asyncHandler( async (req, res) => {
+  let book;
+
+  try {
+    book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect("/books");
+    } else {
+      res.sendStatus(404);
+    }
+  } catch(error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Article.build(req.body);
+      book.id = req.params.id; // make sure correct article gets updated
+      res.render("/books/"+req.params.id, { book, errors: error.errors})
+    } else {
+      throw error;
+    }
+  }
+}));
 
 
 
